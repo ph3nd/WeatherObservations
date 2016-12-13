23c999f0625a373796b411940bff50573d85fc9a
Brendan Cale <brendan.cale87@gmail.com>
2016-12-13 08:15:46 +0000

    Removed code for 2nd lux sensor

--- TSL2561-b.py
+++ TSL2561-b.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import sys
 import smbus
 import time
 import datetime
-import gspread
-from Adafruit_I2C import Adafruit_I2C
+import Adafruit_GPIO.I2C as I2C
 
 # ===========================================================================
 # Program to log the output of two TLS2561 Luxmeters on the same extended i2c bus.
@@ -18,11 +17,6 @@ from Adafruit_I2C import Adafruit_I2C
 #
 # Google Account Details
 # ===========================================================================
-
-# Account details for google docs
-email       = 'some.user@gmail.com'
-password    = 'somesecret'
-spreadsheet = 'Rooftop_Lux'
 
 ####################################
 # address param for Luxmeter instance is set by ADDR pin connection: 
@@ -39,27 +33,12 @@ spreadsheet = 'Rooftop_Lux'
 #  0x12 = 16X gain, 402 mSec (darkest)
 #######################################
 #
-# Login with your Google account
-try:
-  gc = gspread.login(email, password)
-except:
-  print "Unable to log in.  Check your email address/password"
-  sys.exit()
-
-# Open a worksheet from your spreadsheet using the filename
-try:
-  worksheet = gc.open(spreadsheet).sheet1
-  # Alternatively, open a spreadsheet using the spreadsheet's key
-  # worksheet = gc.open_by_key('0ApwDUb5fnx_XdENOV1Rncmpac25jMkd3d1lEM1RKUnc')
-except:
-  print "Unable to open the spreadsheet.  Check your filename: %s" % spreadsheet
-  sys.exit()
 
 class Luxmeter :
   i2c = None
 
   def __init__(self, address=0x39, debug=0):
-    self.i2c = Adafruit_I2C(address)
+    self.i2c = I2C.get_i2c_device(address)
     self.address = address
     self.debug = debug
 
@@ -70,7 +49,7 @@ class Luxmeter :
     "Reads visible + IR value from the I2C device"
     try:
       fullval = self.i2c.readU16(reg)
-      newval = self.i2c.reverseByteOrder(fullval)
+      newval = I2C.reverseByteOrder(fullval)
       if (self.debug):
         print "I2C: Device 0x%02X returned 0x%04X from reg 0x%02X" % (self.address, fullval & 0xFFFF, reg)
       return newval
@@ -82,7 +61,7 @@ class Luxmeter :
     "Reads IR value from the I2C device"
     try:
       IRval = self.i2c.readU16(reg)
-      newIR = self.i2c.reverseByteOrder(IRval)
+      newIR = I2C.reverseByteOrder(IRval)
       if (self.debug):
         print "I2C: Device 0x%02X returned 0x%04X from reg 0x%02X" % (self.address, IRval & 0xFFFF, reg)
       return newIR
@@ -92,7 +71,7 @@ class Luxmeter :
 
 # Continuously append data from East- and West-facing sensors
 while(True):
-  eastlux = Luxmeter(0x29,False)       # create instance with 0v i2c address and no debug messages
+  eastlux = Luxmeter(0x39, False)       # create instance with 0v i2c address and no debug messages
   eastlux.i2c.write8(0x80, 0x03)     # enable the device
   eastlux.i2c.write8(0x81, 0x11)     # set gain = 16X and timing = 101 mSec
 
@@ -124,47 +103,5 @@ while(True):
   elif (ratio > 1.3):
         elux = 0
 
-  westlux = Luxmeter(0x49,False)      # create instance with 3v3 i2c address and no debug messages
-  westlux.i2c.write8(0x80, 0x03)     # enable the device
-  westlux.i2c.write8(0x81, 0x11)     # set gain = 16X and timing = 101 mSec
+  print "egainsetting: {}, ambient: {}, infra: {}, lux: {}".format(egainsetting, eambient, einfra, elux) 
 
-  wgainsetting = "High gain"
-  wambient = westlux.readfull()
-  if wambient >= 37177:         # sensor is maxed out, so remove the 16X gain
-    westlux.i2c.write8(0x81,0x01)
-    wgainsetting = "Low gain"
-    time.sleep(0.41)
-    wambient = westlux.readfull()    # try again
-
-#  wambient = wambient + fudgefactor   # add offset to match up the two sensors
-
-  time.sleep(0.41)         # allow for slowest A2D conversion
-  winfra = westlux.readIR()
-  time.sleep(0.41)         # allow for slowest A2D conversion
-
-  ratio = (float) (winfra / wambient)
-
-  if ((ratio >= 0) & (ratio <= 0.52)):
-   wlux = (0.0315 * wambient) - (0.0593 * wambient * (ratio**1.4))
-  elif (ratio <= 0.65):
-   wlux = (0.0229 * wambient) - (0.0291 * winfra)
-  elif (ratio <= 0.80):
-    wlux = (0.0157 * wambient) - (0.018 + winfra)
-  elif (ratio <= 1.3):
-    wlux = (0.00338 * wambient) - (0.0026 * winfra)
-  elif (ratio > 1.3):
-        wlux = 0
-
-  if (elux + wlux) > 10:      # don't log in the dark!
-    # Append the data to the spreadsheet, including a timestamp
-    try:
-      values = [datetime.datetime.now(), egainsetting, eambient, einfra, elux, wgainsetting, wambient, winfra, wlux]
-      worksheet.append_row(values)
-    except:
-      print "Unable to append data.  Check your connection?"
-      sys.exit()
-
-    print "East Lux = %.2f, West Lux = %.2f. Wrote a row to %s" % (elux, wlux, spreadsheet)
-
-  # Get one set of readings every three minutes
-  time.sleep(173)
