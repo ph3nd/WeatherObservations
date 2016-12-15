b9f76f6774629eed9d52e220ad71c976f0ccaf11
Brendan Cale <brendan.cale87@gmail.com>
2016-12-15 23:41:14 +0000

    Removed unneeded TSL2561 libraries

--- TSL2561.py
+++ TSL2561.py
@@ -1,113 +1,280 @@
 #!/usr/bin/python
-# Code sourced from AdaFruit discussion board: https://www.adafruit.com/forums/viewtopic.php?f=8&t=34922
+# TSL2561 I2C Light-To-Digital converter library for the Raspberry Pi.
+# Datasheet: https://www.adafruit.com/datasheets/TSL2561.pdf
+#
+# -- Cedric Maion, 2016
 
-
-import sys
 import time
-#Adafruit_I2C from https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code/blob/master/Adafruit_I2C/Adafruit_I2C.py
 import Adafruit_GPIO.I2C as I2C
+import RPi.GPIO as GPIO
+
+TSL2561_Control = 0x80
+TSL2561_Timing = 0x81
+TSL2561_Interrupt = 0x86
+TSL2561_Channel0L = 0x8C
+TSL2561_Channel0H = 0x8D
+TSL2561_Channel1L = 0x8E
+TSL2561_Channel1H = 0x8F
+
+TSL2561_Address = 0x39 #device address
+
+LUX_SCALE = 14 # scale by 2^14
+RATIO_SCALE = 9 # scale ratio by 2^9
+CH_SCALE = 10 # scale channel values by 2^10
+CHSCALE_TINT0 = 0x7517 # 322/11 * 2^CH_SCALE
+CHSCALE_TINT1 = 0x0fe7 # 322/81 * 2^CH_SCALE
+
+K1T = 0x0040 # 0.125 * 2^RATIO_SCALE
+B1T = 0x01f2 # 0.0304 * 2^LUX_SCALE
+M1T = 0x01be # 0.0272 * 2^LUX_SCALE
+K2T = 0x0080 # 0.250 * 2^RATIO_SCA
+B2T = 0x0214 # 0.0325 * 2^LUX_SCALE
+M2T = 0x02d1 # 0.0440 * 2^LUX_SCALE
+K3T = 0x00c0 # 0.375 * 2^RATIO_SCALE
+B3T = 0x023f # 0.0351 * 2^LUX_SCALE
+M3T = 0x037b # 0.0544 * 2^LUX_SCALE
+K4T = 0x0100 # 0.50 * 2^RATIO_SCALE
+B4T = 0x0270 # 0.0381 * 2^LUX_SCALE
+M4T = 0x03fe # 0.0624 * 2^LUX_SCALE
+K5T = 0x0138 # 0.61 * 2^RATIO_SCALE
+B5T = 0x016f # 0.0224 * 2^LUX_SCALE
+M5T = 0x01fc # 0.0310 * 2^LUX_SCALE
+K6T = 0x019a # 0.80 * 2^RATIO_SCALE
+B6T = 0x00d2 # 0.0128 * 2^LUX_SCALE
+M6T = 0x00fb # 0.0153 * 2^LUX_SCALE
+K7T = 0x029a # 1.3 * 2^RATIO_SCALE
+B7T = 0x0018 # 0.00146 * 2^LUX_SCALE
+M7T = 0x0012 # 0.00112 * 2^LUX_SCALE
+K8T = 0x029a # 1.3 * 2^RATIO_SCALE
+B8T = 0x0000 # 0.000 * 2^LUX_SCALE
+M8T = 0x0000 # 0.000 * 2^LUX_SCALE
 
 
-### Written for Python 2 <-!!!
-### Big thanks to bryand, who wrote the code that I borrowed heavily from/was inspired by
-### More thanks pandring who kind of kickstarted my work on the TSL2561 sensor
-### A great big huge thanks to driverblock and the Adafruit team (Congrats on your many succeses
-### Ladyada).  Without you folks I would just be a guy sitting somewhere thinking about cool stuff
-### Now I'm a guy building cool stuff.
-### If any of this code proves useful, drop me a line at medicforlife.blogspot.com
 
-# TODO: Strip out values into constants. 
+K1C = 0x0043 # 0.130 * 2^RATIO_SCALE
+B1C = 0x0204 # 0.0315 * 2^LUX_SCALE
+M1C = 0x01ad # 0.0262 * 2^LUX_SCALE
+K2C = 0x0085 # 0.260 * 2^RATIO_SCALE
+B2C = 0x0228 # 0.0337 * 2^LUX_SCALE
+M2C = 0x02c1 # 0.0430 * 2^LUX_SCALE
+K3C = 0x00c8 # 0.390 * 2^RATIO_SCALE
+B3C = 0x0253 # 0.0363 * 2^LUX_SCALE
+M3C = 0x0363 # 0.0529 * 2^LUX_SCALE
+K4C = 0x010a # 0.520 * 2^RATIO_SCALE
+B4C = 0x0282 # 0.0392 * 2^LUX_SCALE
+M4C = 0x03df # 0.0605 * 2^LUX_SCALE
+K5C = 0x014d # 0.65 * 2^RATIO_SCALE
+B5C = 0x0177 # 0.0229 * 2^LUX_SCALE
+M5C = 0x01dd # 0.0291 * 2^LUX_SCALE
+K6C = 0x019a # 0.80 * 2^RATIO_SCALE
+B6C = 0x0101 # 0.0157 * 2^LUX_SCALE
+M6C = 0x0127 # 0.0180 * 2^LUX_SCALE
+K7C = 0x029a # 1.3 * 2^RATIO_SCALE
+B7C = 0x0037 # 0.00338 * 2^LUX_SCALE
+M7C = 0x002b # 0.00260 * 2^LUX_SCALE
+K8C = 0x029a # 1.3 * 2^RATIO_SCALE
+B8C = 0x0000 # 0.000 * 2^LUX_SCALE
+M8C = 0x0000 # 0.000 * 2^LUX_SCALE
 
-class TSL2561:
-    i2c = None
+class LuxMeter:
+    def __init__(self):
+        self._debug = False 
+        self._packageType = 0 # 0=T package, 1=CS package
+        self._gain = 0        # current self._gain: 0=1x, 1=16x [dynamically selected]
+        self._gain_m = 1      # current self._gain, as multiplier
+        self._timing = 0      # current integration time: 0=13.7ms, 1=101ms, 2=402ms [dynamically selected]
+        self._timing_ms = 0   # current integration time, in ms
+        self._channel0 = 0    # raw current value of visible+ir sensor
+        self._channel1 = 0    # raw current value of ir sensor
+        self._schannel0 = 0   # normalized current value of visible+ir sensor
+        self._schannel1 = 0   # normalized current value of ir sensor
 
-    def __init__(self, address=0x39, debug=0, pause=0.8):
-        self.i2c = I2C.get_i2c_device(address)
-        self.address = address
-        self.pause = pause
-        self.debug = debug
-        self.gain = 0 # no gain preselected
-        self.i2c.write8(0x80, 0x03)     # enable the device
+        self._i2c = I2C.get_i2c_device(TSL2561_Address)
 
+        self.powerUp()
+        self.setTintAndGain()
+        self._writeRegister(TSL2561_Interrupt, 0x00)
+        self.powerDown()
 
-    def setGain(self,gain=1):
-        """ Set the gain """
-        if (gain != self.gain):
-            if (gain==1):
-                self.i2c.write8(0x81, 0x02)     # set gain = 1X and timing = 402 mSec
-                if (self.debug):
-                    print "Setting low gain"
-            else:
-                self.i2c.write8(0x81, 0x12)     # set gain = 16X and timing = 402 mSec
-                if (self.debug):
-                    print "Setting high gain"
-            self.gain=gain;                     # safe gain for calculation
-            time.sleep(self.pause)              # pause for integration (self.pause must be bigger than integration time)
-
-
-    def readWord(self, reg):
-        """Reads a word from the I2C device"""
+    def _readRegister(self, address):
         try:
-            wordval = self.i2c.readU16(reg)
-            newval = I2C.reverseByteOrder(wordval)
-            if (self.debug):
-                print("I2C: Device 0x%02X returned 0x%04X from reg 0x%02X" % (self.address, wordval & 0xFFFF, reg))
-            return newval
+            byteval = self._i2c.readU8(address)
+            if (self._debug):
+                print("TSL2561.self._readRegister: returned 0x%02X from reg 0x%02X" % (byteval, address))
+            return byteval
         except IOError:
-            print("Error accessing 0x%02X: Check your I2C address" % self.address)
+            print("TSL2561.self._readRegister: error reading byte from reg 0x%02X" % address)
             return -1
 
+    def _writeRegister(self, address, val):
+        try:
+            self._i2c.write8(address, val)
+            if (self._debug):
+                print("TSL2561.self._writeRegister: wrote 0x%02X to reg 0x%02X" % (val, address))
+        except IOError:
+            print("TSL2561.self._writeRegister: error writing byte to reg 0x%02X" % address)
+            return -1
 
-    def readFull(self, reg=0x8C):
-        """Reads visible+IR diode from the I2C device"""
-        return self.readWord(reg);
+    def powerUp(self):
+        self._writeRegister(TSL2561_Control, 0x03)
 
-    def readIR(self, reg=0x8E):
-        """Reads IR only diode from the I2C device"""
-        return self.readWord(reg);
+    def powerDown(self):
+        self._writeRegister(TSL2561_Control, 0x00)
 
-    def readLux(self, gain = 0):
-        """Grabs a lux reading either with autoranging (gain=0) or with a specified gain (1, 16)"""
-        if (gain == 1 or gain == 16):
-            self.setGain(gain) # low/highGain
-            ambient = self.readFull()
-            IR = self.readIR()
-        elif (gain==0): # auto gain
-            self.setGain(16) # first try highGain
-            ambient = self.readFull()
-            if (ambient < 65535):
-                IR = self.readIR()
-            if (ambient >= 65535 or IR >= 65535): # value(s) exeed(s) datarange
-                self.setGain(1) # set lowGain
-                ambient = self.readFull()
-                IR = self.readIR()
+    def setTintAndGain(self):
+        if self._gain == 0:
+            self._gain_m = 1
+        else:
+            self._gain_m = 16
 
-        if (self.gain==1):
-           ambient *= 16    # scale 1x to 16x
-           IR *= 16         # scale 1x to 16x
-                        
-        ratio = (IR / float(ambient)) # changed to make it run under python 2
+        if self._timing == 0:
+            self._timing_ms = 13.7
+        elif self._timing == 1:
+            self._timing_ms = 101
+        else:
+            self._timing_ms = 402
+        self._writeRegister(TSL2561_Timing, self._timing | self._gain << 4)
 
-        if (self.debug):
-            print "IR Result", IR
-            print "Ambient Result", ambient
+    def readLux(self):
+        time.sleep(float(self._timing_ms + 1) / 1000)
 
-        if ((ratio >= 0) & (ratio <= 0.52)):
-            lux = (0.0315 * ambient) - (0.0593 * ambient * (ratio**1.4))
-        elif (ratio <= 0.65):
-            lux = (0.0229 * ambient) - (0.0291 * IR)
-        elif (ratio <= 0.80):
-            lux = (0.0157 * ambient) - (0.018 * IR)
-        elif (ratio <= 1.3):
-            lux = (0.00338 * ambient) - (0.0026 * IR)
-        elif (ratio > 1.3):
-            lux = 0
+        ch0_low  = self._readRegister(TSL2561_Channel0L)
+        ch0_high = self._readRegister(TSL2561_Channel0H)
+        ch1_low  = self._readRegister(TSL2561_Channel1L)
+        ch1_high = self._readRegister(TSL2561_Channel1H)
 
-        return (lux, ambient, IR)
+        self._channel0 = (ch0_high<<8) | ch0_low
+        self._channel1 = (ch1_high<<8) | ch1_low
+
+        if self._debug:
+            print("TSL2561.readVisibleLux: channel 0 = %i, channel 1 = %i [self._gain=%ix, self._timing=%ims]" % (self._channel0, self._channel1, self._gain_m, self._timing_ms))
+
+    def readVisibleLux(self):
+        self.powerUp()
+        self.readLux()
+
+        if self._channel0 < 500 and self._timing == 0:
+            self._timing = 1
+            if self._debug:
+                print("TSL2561.readVisibleLux: too dark. Increasing integration time from 13.7ms to 101ms")
+            self.setTintAndGain()
+            self.readLux()
+
+        if self._channel0 < 500 and self._timing == 1:
+            self._timing = 2
+            if self._debug:
+                print("TSL2561.readVisibleLux: too dark. Increasing integration time from 101ms to 402ms")
+            self.setTintAndGain()
+            self.readLux()
+
+        if self._channel0 < 500 and self._timing == 2 and self._gain == 0:
+            self._gain = 1
+            if self._debug:
+                print("TSL2561.readVisibleLux: too dark. Setting high self._gain")
+            self.setTintAndGain()
+            self.readLux()
+
+        if (self._channel0 > 20000 or self._channel1 > 20000) and self._timing == 2 and self._gain == 1:
+            self._gain = 0
+            if self._debug:
+                print("TSL2561.readVisibleLux: enough light. Setting low self._gain")
+            self.setTintAndGain()
+            self.readLux()
+
+        if (self._channel0 > 20000 or self._channel1 > 20000) and self._timing == 2:
+            self._timing = 1
+            if self._debug:
+                print("TSL2561.readVisibleLux: enough light. Reducing integration time from 402ms to 101ms")
+            self.setTintAndGain()
+            self.readLux()
+
+        if (self._channel0 > 10000 or self._channel1 > 10000) and self._timing == 1:
+            self._timing = 0
+            if self._debug:
+                print("TSL2561.readVisibleLux: enough light. Reducing integration time from 101ms to 13.7ms")
+            self.setTintAndGain()
+            self.readLux()
+
+        self.powerDown()
+
+        if (self._timing == 0 and (self._channel0 > 5000 or self._channel1 > 5000)) \
+            or (self._timing == 1 and (self._channel0 > 37000 or self._channel1 > 37000)) \
+            or (self._timing == 2 and (self._channel0 > 65000 or self._channel1 > 65000)):
+            # overflow
+            return -1
+
+        return self.calculateLux(self._channel0, self._channel1)
+
+    def calculateLux(self, ch0, ch1):
+        chScale = 0
+        if self._timing == 0:   # 13.7 msec
+            chScale = CHSCALE_TINT0
+        elif self._timing == 1: # 101 msec
+            chScale = CHSCALE_TINT1;
+        else:           # assume no scaling
+            chScale = (1 << CH_SCALE)
+
+        if self._gain == 0:
+            chScale = chScale << 4 # scale 1X to 16X
+
+        # scale the channel values
+        self._schannel0 = (ch0 * chScale) >> CH_SCALE
+        self._schannel1 = (ch1 * chScale) >> CH_SCALE
+
+        ratio = 0
+        if self._schannel0 != 0:
+            ratio = (self._schannel1 << (RATIO_SCALE+1)) / self._schannel0
+        ratio = (ratio + 1) >> 1
+
+        if self._packageType == 0: # T package
+            if ((ratio >= 0) and (ratio <= K1T)):
+                b=B1T; m=M1T;
+            elif (ratio <= K2T):
+                b=B2T; m=M2T;
+            elif (ratio <= K3T):
+                b=B3T; m=M3T;
+            elif (ratio <= K4T):
+                b=B4T; m=M4T;
+            elif (ratio <= K5T):
+                b=B5T; m=M5T;
+            elif (ratio <= K6T):
+                b=B6T; m=M6T;
+            elif (ratio <= K7T):
+                b=B7T; m=M7T;
+            elif (ratio > K8T):
+                b=B8T; m=M8T;
+        elif self._packageType == 1: # CS package
+            if ((ratio >= 0) and (ratio <= K1C)):
+                b=B1C; m=M1C;
+            elif (ratio <= K2C):
+                b=B2C; m=M2C;
+            elif (ratio <= K3C):
+                b=B3C; m=M3C;
+            elif (ratio <= K4C):
+                b=B4C; m=M4C;
+            elif (ratio <= K5C):
+                b=B5C; m=M5C;
+            elif (ratio <= K6C):
+                b=B6C; m=M6C;
+            elif (ratio <= K7C):
+                b=B7C; m=M7C;
+
+        temp = ((self._schannel0*b)-(self._schannel1*m))
+        if temp < 0:
+            temp = 0;
+        temp += (1<<(LUX_SCALE-1))
+        # strip off fractional portion
+        lux = temp>>LUX_SCALE
+        if self._debug:
+            print("TSL2561.calculateLux: %i" % lux)
+
+        return lux
+
+def main():
+    lux = LuxMeter()
+    while (True):
+        print "Lux: %i [vis+ir=%i, ir=%i @ self._gain=%ix, self._timing=%.1fms]" % (lux.readVisibleLux(), lux._channel0, lux._channel1, lux._gain_m, lux._timing_ms)
+        time.sleep(1)
 
 if __name__ == "__main__":
-    tsl=TSL2561()
-    print tsl.readLux(1)
-#print "LUX HIGH GAIN ", tsl.readLux(16)
-#print "LUX LOW GAIN ", tsl.readLux(1)
-#print "LUX AUTO GAIN ", tsl.readLux()
+        main()
