9e88955d6b75d2e773486fadb398779880b6dbb3
Brendan Cale <brendan.cale87@gmail.com>
2016-12-23 09:46:02 +0000

    fixed some bugs

--- sense.py
+++ sense.py
@@ -6,47 +6,64 @@ from BMP280 import BMP280
 from SI7021 import SI7021
 import requests
 import csv
+import time
+import json
 
 class Observe:
     def __init__(self):
         self._luxMeter = LuxMeter()
-        self._lux = 0
-        self._ambient = 0
-        self._ifraRed = 0
-
         self._presMeter = BMP280()
-        self._pressure = 0
-        self._tempPres = 0
-
         self._humMeter = SI7021()
-        self._humidity = 0
-        self._tempHum = 0
+
+        self._observation = None
 
     def GetObservations(self):
-        self._lux, self._ambient, self._infraRed = self._luxMeter.readVisibleLux()
-        
-        self._pressure = self._presMeter.GetPressure()
-        self._tempPres = self._presMeter.GetTemperature()
+        obs = dict()
+        lux = 0
+        ambient = 0
+        infraRed = 0
 
-        self._humidity = self._humMeter.GetRHumidity()
-        self._tempHum = self._humMeter.GetTemperature()
+        timestamp = int(time.time())
+        lux, ambient, infraRed = self._luxMeter.readVisibleLux()
+
+        pressure = self._presMeter.GetPressure()
+        tempPres = self._presMeter.GetTemperature()
+        altitude = self._presMeter.GetAltitude()
+
+        humidity = self._humMeter.GetRHumidity()
+        tempHum = self._humMeter.GetTemperature()
+
+        obs["temp"] = tempHum
+        obs["pres"] = pressure
+        obs["rhum"] = humidity
+        
+        luxd = dict()
+        luxd["luxd"] = lux
+        luxd["ambient"] = ambient
+        luxd["infrared"] = infraRed
+
+        obs["lux"] = {}
+        obs["lux"] = luxd
+        obs["alt"] = altitude
+        obs["time"] = timestamp
+        obs["raw"] = dict(obs)
+        obs["raw"]["temp2"] = tempPres
+
+        self._observation = WeatherObservation(obs)
+        return self._observation.getObservation()
 
     def WriteCSV(self):
         pass
 
     def PostObservations(self):
-        pass
-
+        r = requests.post('http://localhost:80/api/addobservation', data=json.dumps(self._observation.getObservation()), headers={'Content-type':'application/json',})
 
 def sense():
     obs = Observe()
-
-    obs.GetObservations()
-
-    print "Lux: {}, Ambient {}, Infra Red {}".format(obs._lux, obs._ambient, obs._infraRed)
-    print "Pressure {}, Temperature {}".format(obs._pressure, obs._tempPres)
-    print "Relative Humidity {}, Temperature {}".format(obs._humidity, obs._tempHum)
-
+    x = obs.GetObservations()
+    obs.PostObservations()
 
 if __name__ == "__main__":
-    sense() 
+    while True:
+        sense()
+        time.sleep(30)
